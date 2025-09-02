1d1139e391cca629bad16ba210aec7433106065e
omarrui <rouiguio919@gmail.com>
2025-09-02 10:51:06 +0200

    tasks

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -1,18 +1,13 @@
 #!/usr/bin/python3
 import sys
 
+
 def factorial(n):
-    """
-    calculate the factorial recursivly
-    the factorial nums r from 1 to n
-    with 0 an exception
-    """
     if n == 0:
         return 1
     else:
-        return n * factorial(n - 1)
+        return n * factorial(n-1)
 
-number = int(sys.argv[1])
 
-result = factorial(number)
-print(result)
+f = factorial(int(sys.argv[1]))
+print(f)
